ee718953072f9d31ce5e3441bacb0a7766f16471
tanya <tanya.svechka@gmail.com>
2024-01-14 17:56:15 +0200

    home work done.

--- home_work2.py
+++ home_work2.py
@@ -7,6 +7,20 @@ print(outside_input // 1000)
 print((outside_input % 1000) // 100)
 print((outside_input % 100) // 10)
 print(outside_input % 10)
+# #
+# #
+# # Using divmod method.
+# # # A request to add digits
+# # outside_input = int(input("Dare to add 4 random digits: "))
+# #
+# # # Extraction process
+# # thousands, left = divmod(outside_input, 1000)
+# # hundreds,left = divmod(left, 100)
+# # tens,ones = divmod(left, 10)
+# #
+# # # Results printing
+# # print(thousands)
+# # print(hundreds)
+# # print(tens)
+# # print(ones)
 
-
-print('Home work done, at a girl!')
