19002540a372560d8a8721d0d5422ea04d896785
tanya <tanya.svechka@gmail.com>
2024-01-14 18:11:56 +0200

    home work done.

--- home_work2.py
+++ home_work2.py
@@ -1,14 +1,14 @@
-#
-# # A request to add digits
-# outside_input = int(input("Dare to add 4 random digits: "))
-#
-# # Printing the results using the modulo operator % and the floor division operator // to extract digits
-# print(outside_input // 1000)
-# print((outside_input % 1000) // 100)
-# print((outside_input % 100) // 10)
-# print(outside_input % 10)
-# # #
+
+# A request to add digits
+outside_input = int(input("Dare to add 4 random digits: "))
+
+# Printing the results using the modulo operator % and the floor division operator // to extract digits
+print(outside_input // 1000)
+print((outside_input % 1000) // 100)
+print((outside_input % 100) // 10)
+print(outside_input % 10)
 # #
+#
 # A request to add digits
 outside_input = int(input("Dare to add 4 random digits: "))
 
