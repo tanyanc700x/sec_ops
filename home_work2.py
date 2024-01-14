ee8ea2c7f5b7f74b1808b7809c25407f23d0650d
tanya <tanya.svechka@gmail.com>
2024-01-14 18:09:13 +0200

    home work done.

--- home_work2.py
+++ home_work2.py
@@ -1,26 +1,25 @@
-
+#
+# # A request to add digits
+# outside_input = int(input("Dare to add 4 random digits: "))
+#
+# # Printing the results using the modulo operator % and the floor division operator // to extract digits
+# print(outside_input // 1000)
+# print((outside_input % 1000) // 100)
+# print((outside_input % 100) // 10)
+# print(outside_input % 10)
+# # #
+# #
 # A request to add digits
 outside_input = int(input("Dare to add 4 random digits: "))
 
-# Printing the results using the modulo operator % and the floor division operator // to extract digits
-print(outside_input // 1000)
-print((outside_input % 1000) // 100)
-print((outside_input % 100) // 10)
-print(outside_input % 10)
-# #
-# #
-# # Using divmod method.
-# # # A request to add digits
-# # outside_input = int(input("Dare to add 4 random digits: "))
-# #
-# # # Extraction process
-# # thousands, left = divmod(outside_input, 1000)
-# # hundreds,left = divmod(left, 100)
-# # tens,ones = divmod(left, 10)
-# #
-# # # Results printing
-# # print(thousands)
-# # print(hundreds)
-# # print(tens)
-# # print(ones)
+# Extraction process
+thousands, left1 = divmod(outside_input, 1000)
+hundreds, left2 = divmod(left1, 100)
+tens, ones = divmod(left2, 10)
+
+# Results printing
+print(thousands)
+print(hundreds)
+print(tens)
+print(ones)
 
