625b74344859a3e3befc332187283acf42c0b557
tanya <tanya.svechka@gmail.com>
2024-01-14 23:57:19 +0200

    home work done.

--- Tests.py
+++ Tests.py
@@ -1,63 +1,12 @@
-# # Get user input
-# user_input = input("Dare to add 4 random digits: ")
-#
-# # Check if the input consists of only digits and is a valid integer
-# if user_input.isdigit():
-#     # Convert input to an integer
-#     outside_input = int(user_input)
-#
-#     # Extract and print the digits
-#     thousands, remainder = divmod(outside_input, 1000)
-#     hundreds, remainder = divmod(remainder, 100)
-#     tens, ones = divmod(remainder, 10)
-#
-#     # Printing the results
-#     print("Thousands:", thousands)
-#     print("Hundreds:", hundreds)
-#     print("Tens:", tens)
-#     print("Ones:", ones)
-#
-#     print('Homework done, at a girl!')
-# else:
-#     print("Please digits, not letters or words.")
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-# # from decimal import Decimal
-# #
-# # # Creation of Decimal objects
-# # x = Decimal('10.0985')
-# # y = Decimal('3.7895432')
-# #
-# # # Arithmetic operations with Decimal
-# # sum_result = x + y
-# # difference_result = x - y
-# # product_result = x * y
-# # quotient_result = x / y
-# #
-# # # Results and type check
-# # print("Sum:", sum_result, type(sum_result))
-# # print("Difference:", difference_result)
-# # print("Product:", product_result)
-# # print("Quotient:", quotient_result)
-# #
-# #
-# #
-# # number_1 = 5
-# # number_2 = number_1
-# #
-# # print (number_1)
-# #
-# # number_1 = 3
-# # print(number_2, type(number_1))
+# outside_input = int(input("Dare to add 4 random digits: "))
+# for x in str(outside_input):
+#     print(x)
 
 
+outside_input = int(input("Dare to add 4 random digits: "))
 
+# Получаем каждую цифру числа и выводим её
+while outside_input > 0:
+    digit = outside_input % 10
+    print(digit)
+    outside_input //= 10
