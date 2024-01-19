91095c5e9a193a640ddfdde2c46292fe9469f13c
tanya <tanya.svechka@gmail.com>
2024-01-19 18:12:41 +0200

    home work done.

--- Perebor.py
+++ Perebor.py
@@ -1,12 +1,25 @@
 example1 = [12, 3, 4, 10]
-example1 = [1]
-example1 = []
+# example1 = [1]
+# example1 = []
 
-if example1:
+if len(example1) > 1:
     last_element = example1.pop(-1)
     example1.insert(0, last_element)
 else:
-
     pass
-
 print(example1)
+######################################### Variation ############################################################
+
+# example1 = [12, 3, 4, 10]
+# example1 = [1]
+# example1 = []
+#
+# if example1:
+#     last_element = example1.pop(-1)
+#     example1.insert(0, last_element)
+# else:
+#
+#     pass
+#
+# print(example1)
+
