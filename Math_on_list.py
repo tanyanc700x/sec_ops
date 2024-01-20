4e648c2e6e6aae15f77f38970ea0bf3b906c0f72
tanya <tanya.svechka@gmail.com>
2024-01-20 09:37:49 +0200

    home work done.

---
+++ Math_on_list.py
@@ -0,0 +1,49 @@
+my_list = [0, 1, 7, 2, 4, 8]
+# my_list = [1, 3, 5]
+# my_list = [6]
+# my_list = [0]
+result = my_list[0::2]
+print(result)
+print(len(result))
+#
+math = sum(x for x in result) * my_list[-1]
+print(math)
+
+########################################
+
+
+# if len(my_list) > 1:
+#     result = my_list[0::2]
+#     print(result)
+#     print(len(result))
+#     multiplication = (int(result[0]) + int(result[1]) +  int(result[2]) +  int(result[3]) + int(result[4])) * int(result[-1])
+#     print(multiplication)
+# else:
+
+
+
+
+
+
+
+
+my_list = [0, 1, 7, 2, 4, 8]
+
+
+
+
+
+
+################################### for loop ######################################################
+
+
+# my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# result = my_list[0::2]
+# print(result)
+# product = 1
+# for num in result:
+#     product = product * int(num)
+#
+# print(product)
+
+################################### Wile loop ######################################################
