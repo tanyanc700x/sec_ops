abae50c33d994d5baa124606cc81c1b58943874f
tanya <tanya.svechka@gmail.com>
2024-01-24 06:20:20 +0200

    home work done.

--- Shyfr.py
+++ Shyfr.py
@@ -1,14 +1,14 @@
-alphabet = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
-shyfr = ("хтёиц тц лсдотртжт. тфштжфдшмг хтщфдсисд. ечзмь ёд шфдсяои, сд ёдолдпи емпгьм тымса ёочхсяи. емфм хфдлч "
+letters = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
+shyfr = ("хтёиц тц лсдотртжт. тфштжфдшмг хтщфдсисд: ечзмь ёд шфдсяои - сд ёдолдпи емпгьм тымса ёочхсяи. емфм хфдлч "
          "утпцтфд. дзмс си уфдычхцёчиь. зёд смиь. зёд - пмцдпасд, удпцдфд - стфр!")
 key = -4
 result = ""
 
 for any_char in shyfr:
-    if any_char in alphabet:
-        position = alphabet.index(any_char)
-        newPosition = (position + key) % len(alphabet)
-        result += alphabet[newPosition]
+    if any_char in letters:
+        char_place = letters.index(any_char)
+        char_place_2 = (char_place + key) % len(letters)
+        result += letters[char_place_2]
     else:
         result += any_char
 
