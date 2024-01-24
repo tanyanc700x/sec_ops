41f4900fb0b6551938e432ea424ca53feb055e0c
tanya <tanya.svechka@gmail.com>
2024-01-24 06:12:42 +0200

    home work done.

--- Shyfr.py
+++ Shyfr.py
@@ -1,15 +1,17 @@
 alphabet = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
-encrypt = ("хтёиц тц лсдотртжт. тфштжфдшмг хтщфдсисд. ечзмь ёд шфдсяои, сд ёдолдпи емпгьм тымса ёочхсяи. емфм хфдлч утпцтфд. дзмс си уфдычхцёчиь. зёд смиь. зёд - пмцдпасд, удпцдфд - стфр!")
+shyfr = ("хтёиц тц лсдотртжт. тфштжфдшмг хтщфдсисд. ечзмь ёд шфдсяои, сд ёдолдпи емпгьм тымса ёочхсяи. емфм хфдлч "
+         "утпцтфд. дзмс си уфдычхцёчиь. зёд смиь. зёд - пмцдпасд, удпцдфд - стфр!")
 key = -4
-encrypt = encrypt.lower()
-encrypted = ""
+result = ""
 
-for my_char in encrypt:
-    if my_char  in alphabet:
-        position = alphabet.index(my_char )
+for any_char in shyfr:
+    if any_char in alphabet:
+        position = alphabet.index(any_char)
         newPosition = (position + key) % len(alphabet)
-        encrypted += alphabet[newPosition]
+        result += alphabet[newPosition]
     else:
-        encrypted += my_char
+        result += any_char
 
-print("Зашифрованный текст:", encrypted)
+print(result)
+
+
