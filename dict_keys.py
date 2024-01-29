329c34a1a9d0b9dac0f0b7f2dfad4dfdba5f48c1
tanya <tanya.svechka@gmail.com>
2024-01-29 20:54:23 +0200

    corrected result after code review

--- dict_keys.py
+++ dict_keys.py
@@ -1,22 +1,51 @@
-dict_1 = {1: 1, 2: 2, 3: 3, 4: 4}
-dict_2 = {2: 22, 3: 33, 5: 55}
+######################### Sets 1 ##############################
 
-# а) Створити список із ключів, які є в обох словниках.
-common = dict_1.keys() & dict_2.keys()
+x = {1: 1, 2: 2, 3: 3, 4: 4}
+y = {2: 22, 3: 33, 5: 55}
+merge = {}
+
+z = set(x.keys()).difference(y.keys())
+print(z)
+
+common = set(x.keys()).intersection(y.keys())
 print(common)
 
-# б) Створити список із ключів, які є у першому, але немає у другому словнику.
-absent = dict_1.keys() - dict_2.keys()
-print(absent)
 
-# в) Створити новий словник з пар {ключ:значення} для ключів, які є в першому, але немає в другому словнику.
-pairs = {key: dict_1[key] for key in absent}
-print(pairs)
+w = {key: x[key] for key in set(x.keys()).difference(y.keys())}
+print(w)
 
-# г) Об'єднати ці два словники у новий словник за правилом
-merged = {key: [dict_1[key], dict_2[key]] if key in dict_2 else dict_1[key] for key in dict_1.keys()}
-print(merged)
-# print(type(merged))
+for key in set(x.keys()).union(y.keys()):
+    if key in x and key in y:
+        merge[key] = [x[key], y[key]]
+    elif key in x:
+        merge[key] = x[key]
+    elif key in y:
+        merge[key] = y[key]
+
+print(merge)
+
+################################ Sets 2##################################################
+
+
+# dict_1 = {1: 1, 2: 2, 3: 3, 4: 4}
+# dict_2 = {2: 22, 3: 33, 5: 55}
+#
+# # а) Створити список із ключів, які є в обох словниках.
+# common = dict_1.keys() & dict_2.keys()
+# print(common)
+#
+# # б) Створити список із ключів, які є у першому, але немає у другому словнику.
+# absent = dict_1.keys() - dict_2.keys()
+# print(absent)
+#
+# # в) Створити новий словник з пар {ключ:значення} для ключів, які є в першому, але немає в другому словнику.
+# pairs = {key: dict_1[key] for key in absent}
+# print(pairs)
+#
+# # г) Об'єднати ці два словники у новий словник за правилом
+# merged = {key: [dict_1[key], dict_2[key]] if key in dict_2 else dict_1[key] for key in dict_1.keys()}
+# print(merged)
+# # print(type(merged))
 
 
 
