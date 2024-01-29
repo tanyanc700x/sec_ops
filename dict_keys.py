2444b9d5b28ea5acf6e9d61f87c3d7258212338b
tanya <tanya.svechka@gmail.com>
2024-01-29 05:58:07 +0200

    corrected result after code review

---
+++ dict_keys.py
@@ -0,0 +1,21 @@
+dict_1 = {1: 1, 2: 2, 3: 3, 4: 4}
+dict_2 = {2: 22, 3: 33, 5: 55}
+
+# а) Створити список із ключів, які є в обох словниках
+common_keys = list(dict_1.keys() & dict_2.keys())
+
+# б) Створити список із ключів, які є у першому, але немає у другому словнику.
+keys_only_in_first = list(dict_1.keys() - dict_2.keys())
+
+# в) Створити новий словник з пар {ключ:значення} для ключів, які є в першому, але немає в другому словнику.
+new_dict = {key: dict_1[key] for key in keys_only_in_first}
+
+# г) Об'єднати ці два словники у новий словник за правилом
+merged_dict = {key: [dict_1[key], dict_2[key]] if key in dict_2 else dict_1[key] for key in dict_1.keys()}
+
+# Results
+print("а) Список ключів, які є в обох словниках:", common_keys)
+print("б) Список ключів, які є у першому, але немає у другому словнику:", keys_only_in_first)
+print("в) Новий словник з ключів, які є в першому, але немає в другому словнику:", new_dict)
+print("г) Об'єднаний словник:", merged_dict)
+print("Seems like working")
--- dictionary_task.py
+++ dictionary_task.py
@@ -1,24 +1,23 @@
 people_list = [
     {"name": "Galadriel", "age": 6992},
-    {"name": " Fili", "age": 25},
+    {"name": "Fili", "age": 25},
     {"name": "Kili", "age": 25},
     {"name": "Torin", "age": 195},
     {"name": "Legalas", "age": 500},
-    {"name": "Bilbo", "age": 128},
-    {"name": "Saruman", "age": 2400},
-    {"name": "Boromir", "age": 87}
+    {"name": "Saruman", "age": 2400}
+
 ]
 
 # Youngest people search:
 min_age = min(person["age"] for person in people_list)
 youngest_people = [person["name"] for person in people_list if person["age"] == min_age]
-print("The youngest people are:", youngest_people, type(youngest_people))
+print("The youngest people are:", youngest_people)
 
 # Longest name search:
 max_name_length = max(len(person["name"]) for person in people_list)
 longest_names = [person["name"] for person in people_list if len(person["name"]) == max_name_length]
-print("The longest names are:", longest_names, type(longest_names))
+print("The longest name is:", longest_names)
 
 average_age = sum(person["age"] for person in people_list) / len(people_list)
 average_age_list = [average_age]  # конвертуємо float в список
-print("The average age is:", average_age_list, type(average_age_list))
+print("The average age is:", average_age_list)
