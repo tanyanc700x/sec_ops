febaa1de3a08d4e61d4020f300b17ebf1d34d1bc
tanya <tanya.svechka@gmail.com>
2024-02-02 23:33:57 +0200

    correct with list at the end

--- add_one_func.py
+++ add_one_func.py
@@ -4,13 +4,20 @@ def add_one(some_list):
     for digit in some_list:
         number = number * 10 + digit
 
+    updated_list = []  # Move this line up to initialize the list
     number += 1
-    updated_list = str(number)
-    return updated_list
+
+    while number > 0:
+        digit = number % 10
+        updated_list.insert(0, digit)
+        number //= 10
+    return updated_list  # Correct the return statement
+
+
 
 # Test cases
-assert add_one([1, 2, 3, 4]) == '1235', 'Test1'
-assert add_one([9, 9, 9]) == '1000', 'Test2'
-assert add_one([0]) == '1', 'Test3'
-assert add_one([9]) == '10', 'Test4'
-print("OK")
+assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'Test1'
+assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'Test2'
+assert add_one([0]) == [1], 'Test3'
+assert add_one([9]) == [1, 0], 'Test4'
+print("ОК")
