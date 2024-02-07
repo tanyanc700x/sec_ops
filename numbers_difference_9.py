dc0ab390b62d92b0ad005f9425ec6bf35df09527
tanya <tanya.svechka@gmail.com>
2024-02-07 18:09:30 +0200

    should be fine

---
+++ numbers_difference_9.py
@@ -0,0 +1,25 @@
+def difference(*args):
+    if not args:
+        return 0
+    else:
+        max_value = max(args)
+        min_value = min(args)
+        return round(max_value - min_value, 2)
+
+# Перевірка тестів
+assert difference(1, 2, 3) == 2, 'Test1'
+assert difference(5, -5) == 10, 'Test2'
+assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
+assert difference() == 0, 'Test4'
+print('OK')
+
+########################## lambda  ###################################
+# difference = lambda *args: round(max(args, default=0) - min(args, default=0), 2)
+#
+# # Перевірка тестів
+# assert difference(1, 2, 3) == 2, 'Test1'
+# assert difference(5, -5) == 10, 'Test2'
+# assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
+# assert difference() == 0, 'Test4'
+# print('OK')
+
