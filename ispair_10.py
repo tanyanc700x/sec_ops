a344632029d571b27fdaa20526cd3178d1b9f42f
tanya <tanya.svechka@gmail.com>
2024-02-12 13:53:46 +0200

    2 additional check

--- ispair_10.py
+++ ispair_10.py
@@ -6,3 +6,15 @@ assert is_even(2) == True, 'Test1'
 assert is_even(5) == False, 'Test2'
 assert is_even(0) == True, 'Test3'
 print('OK')
+
+
+######################################
+# def is_even(digit):
+#     result = digit % 2 == 0
+#     print(f"Результат для числа {digit}: {result}")
+#     return result
+#
+# assert is_even(2) == True, 'Test1'
+# assert is_even(5) == False, 'Test2'
+# assert is_even(0) == True, 'Test3'
+# print('OK')
