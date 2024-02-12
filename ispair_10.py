5f1367e8dd8e0ca5d443088e6245c7d2685b8aab
tanya <tanya.svechka@gmail.com>
2024-02-12 13:55:18 +0200

    2 additional check

--- ispair_10.py
+++ ispair_10.py
@@ -1,20 +1,20 @@
-def is_even(digit):
-
-    return digit % 2 == 0
-
-assert is_even(2) == True, 'Test1'
-assert is_even(5) == False, 'Test2'
-assert is_even(0) == True, 'Test3'
-print('OK')
-
-
-######################################
 # def is_even(digit):
-#     result = digit % 2 == 0
-#     print(f"Результат для числа {digit}: {result}")
-#     return result
+#
+#     return digit % 2 == 0
 #
 # assert is_even(2) == True, 'Test1'
 # assert is_even(5) == False, 'Test2'
 # assert is_even(0) == True, 'Test3'
 # print('OK')
+
+
+######################################
+def is_even(digit):
+    result = digit % 2 == 0
+    print(f"Result for the digit to check {digit}: {result}")
+    return result
+
+assert is_even(2) == True, 'Test1'
+assert is_even(5) == False, 'Test2'
+assert is_even(0) == True, 'Test3'
+print('OK')
